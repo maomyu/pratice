0821c02f1fb3b70bd6ece2d3c4f547d3361bb417
yuwei1 <yuweijiubayiqi@163.com>
2020-01-18 13:17:25 +0800

    defer

--- pdefer/demo3/demo3.go
+++ pdefer/demo3/demo3.go
@@ -39,6 +39,19 @@ func f4() (r int) {
 	}()
 	return t
 }
+
 func main() {
-	fmt.Println(f4())
+	fmt.Println("return:", test())
+}
+
+func test() (i int) { //返回值命名i
+	defer func() {
+		i++
+		fmt.Println("defer1", i)
+	}()
+	defer func() {
+		i++
+		fmt.Println("defer2", i)
+	}()
+	return i
 }
