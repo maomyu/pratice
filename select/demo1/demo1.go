e92c76851af62b3b92d94381c68229dff27d5134
yuwei1 <yuweijiubayiqi@163.com>
2020-01-21 20:58:37 +0800

    select

---
+++ select/demo1/demo1.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	chan1 := make(chan int)
+	chan2 := make(chan int)
+
+	go func() {
+		chan1 <- 1
+		time.Sleep(5 * time.Second)
+	}()
+
+	go func() {
+		chan2 <- 1
+		time.Sleep(5 * time.Second)
+	}()
+	time.Sleep(10 * time.Second)
+	select {
+	case <-chan1:
+		fmt.Println("chan1 ready.")
+	case <-chan2:
+		fmt.Println("chan2 ready.")
+	default:
+		fmt.Println("default")
+	}
+
+	fmt.Println("main exit.")
+}
